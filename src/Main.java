d79cbadccb51757751e439dd7f8c1f6f5fda015b
Artyom <aroyanart98@gmail.com>
2026-06-22 14:53:16 +0400

    refactor: delete failed code.

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-/**
- * Author: Artyom Aroyan
- * Date: 06.04.26
- * Time: 17:39:07
- */
-void main() {
-}
--- src/main/java/ui/ConsoleUI.java
+++
@@ -1,9 +0,0 @@
-package main.java.ui;
-
-/**
- * Author: Artyom Aroyan
- * Date: 06.04.26
- * Time: 18:12:05
- */
-public class ConsoleUI {
-}
